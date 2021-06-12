687ea19e7065efca367d3838da4a7651133c6565
yefersonahshs <yefersonahshs@gmail.com>
2021-06-12 11:26:39 -0500

    dataBinding

--- app/src/main/java/com/example/cursoandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidkotlin/MainActivity.kt
@@ -2,27 +2,38 @@ package com.example.cursoandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.cursoandroidkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val Age_id:EditText= findViewById<EditText>(R.id.Age_id)
-        val Result_Text:TextView= findViewById<TextView>(R.id.Result_Text)
-        val button:Button= findViewById<Button>(R.id.button)
+        val binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        button.setOnClickListener{
-            val age_string = Age_id.text.toString()
-            val age_int = age_string.toInt()
-            val result = age_int * 7
+        Log.v("Mainactivity" , "main activity verbose ")
+        Log.d("Mainactivity" , "main activity debug")
+        Log.i("Mainactivity" , "main activity info")
+        Log.e("Mainactivity" , "main activity error ")
+        Log.w("Mainactivity" , "main activity warning")
 
-            Result_Text.text="tu edad es  $result en años perros"
+        binding.button.setOnClickListener{
+            val age_string = binding.AgeId.text.toString()
+            if (age_string.isNotEmpty()) {
+                val age_int = age_string.toInt()
+                val result = age_int * 7
 
+                binding.ResultText.text =getString(R.string.result_text,result)
+            }else{
+                Log.d("Mainactivity" , "age is emty ")
+                Toast.makeText(this, R.string.you_must_insert_your_Age , Toast.LENGTH_SHORT).show()
+            }
         }
 
 
